696398d9c6a5322e5ab6cca62f8511bffaa95393
lavanya-106 <34025440+lavanya-106@users.noreply.github.com>
2020-08-01 21:02:14 +0530

    Update app.ts

--- 001 TypeScript - declare variables/before/src/vs2015-TS/src/app.ts
+++ 001 TypeScript - declare variables/before/src/vs2015-TS/src/app.ts
@@ -1,3 +1,16 @@
-﻿
 // Let's get started!
 console.log("Let's get started!");
+
+var variableA: number = 2;
+var variableB: number = 3;
+var variableC: number = 4;
+
+// inferred type
+var inferredvariable = 5;
+
+// no value
+var noInitialValue;
+
+console.log("variableA + variableB + variableC =" + variableA + variableB + variableC);
+console.log("inferredvariable = " + inferredvariable);
+console.log("noInitialValue = " + noInitialValue);
