3014295743852326ea4f836acaf5b8cb07f06f8d
lavanya-106 <34025440+lavanya-106@users.noreply.github.com>
2020-08-01 21:32:53 +0530

    Update app.ts
    declrae variables 2

--- 002 TypeScript - declare variables/before/src/vs2015-TS/src/app.ts
+++ 002 TypeScript - declare variables/before/src/vs2015-TS/src/app.ts
@@ -1,6 +1,21 @@
-﻿
+
+
 // Let's get started!
 console.log("Let's get started!");
 
+var myBool: boolean = true;
+var myNumber: number = 5;
+var myString: string = 'Bob';
+
+var myAnyString: any = 'Joe';
+var myAnyNumber: any = 3;
+var myAnyBool: any = false;
+
+console.log(myBool);
+console.log(myNumber);
+console.log(myString);
+console.log(myAnyString);
+console.log(myAnyNumber);
+console.log(myAnyBool);
 
 
